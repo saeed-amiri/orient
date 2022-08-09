72ca96ac9483ed2f502a807a10f4983d5855ba1e
Saeed Amiri <samiri@uni-bremen.de>
2022-08-09 12:19:04 +0200

    add colors

---
+++ codes/colors_text.py
@@ -0,0 +1,11 @@
+class TextColor:
+    """Defining the color for printing text on console"""
+    BOLD: str = '\033[1m'
+    ENDC: str = '\033[0m'
+    FAIL: str = '\033[91m'
+    HEADER: str = '\033[95m'
+    OKBLUE: str = '\033[94m'
+    OKCYAN: str = '\033[96m'
+    WARNING: str = '\033[93m'
+    OKGREEN: str = '\033[92m'
+    UNDERLINE: str = '\033[4m'
