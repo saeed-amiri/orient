f03b2d044357ba05ee93a0ae55e901d2c3832aa0
Saeed Amiri <samiri@uni-bremen.de>
2022-08-10 14:43:39 +0200

    fix read json based on the combination code

--- codes/read_json.py
+++ codes/read_json.py
@@ -1,4 +1,7 @@
 import json
+import pandas as pd
+from colors_text import TextColor as bcolors
+
 
 class Doc:
     """read parameters file
@@ -8,3 +11,19 @@ class Doc:
     Output:
         type and name of the atoms
         """
+
+
+class ReadJson:
+    """read the json file"""
+    def __init__(self, fname) -> None:
+        print(f'{bcolors.OKCYAN}{self.__class__.__name__}:\n'
+              f'\tReading parameter file: `{fname}`{bcolors.ENDC}\n')
+        self.get_param(fname)
+
+    def get_param(self, fname: str) -> pd.DataFrame:
+        with open(fname, 'r') as f:
+            data = json.load(f)
+        df = pd.DataFrame.from_dict(data["files"][0]['atoms'])
+        self.param = data
+        del data
+        return df
